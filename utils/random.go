d203c98cd1a19a5d3600177ddce9c9342c4286e5
KamingLo <lokaming86@gmail.com>
2026-03-14 19:17:13 +0700

    Newest Version: Adding custom id and fixing the id search

--- utils/random.go
+++ utils/random.go
@@ -8,18 +8,16 @@ import (
 
 // GenerateCustomID menghasilkan format: PREFIX-YYYYMMDD-RANDOM
 func GenerateCustomID(prefix string, length int) string {
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" // Gunakan Uppercase agar seragam di URL
 	datePart := time.Now().Format("20060102")
 
-	b := make([]byte, length)
-	if _, err := rand.Read(b); err != nil {
-		// Fallback jika crypto/rand gagal
-		return fmt.Sprintf("%s-%s-error", prefix, datePart)
+	result := make([]byte, length)
+	// Langsung loop untuk mengisi karakter
+	for i := 0; i < length; i++ {
+		randomByte := make([]byte, 1)
+		rand.Read(randomByte)
+		result[i] = charset[randomByte[0]%uint8(len(charset))]
 	}
 
-	for i := range b {
-		b[i] = charset[int(b[i])%len(charset)]
-	}
-
-	return fmt.Sprintf("%s-%s-%s", prefix, datePart, string(b))
+	return fmt.Sprintf("%s-%s-%s", prefix, datePart, string(result))
 }
